26567acef87bb472a900e5f36eb30104ee366d2c
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-05-26 16:49:58 -0300

    Criado estados no gameoption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,18 +1,20 @@
+import { useState } from "react"
 import styles from "./Game.module.css"
 import GameOption from "../gameOption/GameOption"
 
 function Game(){
+  const [gameState, setGameState] = useState(Array(9).fill(0))
+ 
   return(
     <div className={styles.game}>
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
+      {
+        gameState.map((value,pos)=>{
+          return <GameOption
+            key={`game-option-pos-${pos}`}
+            status={value}
+          />
+        })
+      }
     </div>
   )
 }
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,10 +1,17 @@
 import styles from "./GameOption.module.css"
+import Icon from "../icon/Icon"
 
+const GameIcon = ({iconName}) => <Icon iconName={iconName} size="25px"/>
 
-function GameOption (){
+function GameOption ({status}){
   return(
     <div className={styles.gameOption}>
-      
+      {
+        status === 1 && <Icon iconName="circle"/>
+      }
+      {
+        status === -1 && <Icon iconName="x" />
+      }
     </div>
   )
 }
