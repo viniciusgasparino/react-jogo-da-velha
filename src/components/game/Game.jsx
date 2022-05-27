819a604e8526bbf0808547abf6c0dd2e78cc19e0
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-05-27 16:30:05 -0300

    Criado useEffect

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,20 +1,48 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import styles from "./Game.module.css"
 import Icon from "../icon/Icon"
 import GameOption from "../gameOption/GameOption"
 
+const winnerTable = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function Game(){              
   const [gameState, setGameState] = useState(Array(9).fill(0))
-  const [currentPlayer, setCurrentPlayer] = useState(1)
+  const [currentPlayer, setCurrentPlayer] = useState(-1)
+  const [winner,setWinner] = useState(0)
 
   const handleClick = (pos) => {
-    if(gameState[pos] === 0){
+    if(gameState[pos] === 0 && winner === 0){
       let newGameState = [...gameState]
       newGameState[pos] = currentPlayer
-      setCurrentPlayer(currentPlayer * -1) 
       setGameState(newGameState)
     }
   }
+
+  const verifyGame = () =>{
+    winnerTable.forEach((line)=>{
+        const values = line.map((pos)=> gameState[pos]) 
+        const som = values.reduce((accu,value)=> accu + value)
+        if(som === 3 || som === -3) {
+          setWinner(som/3)
+          console.log("Vencedor" + som/3)
+        }
+      })
+  }
+
+  useEffect(()=>{
+    setCurrentPlayer(currentPlayer * -1) 
+    verifyGame()
+  },[gameState])
+
 
   return(
     <div className={styles.gameContent}>
@@ -34,7 +62,7 @@ function Game(){
          {
            currentPlayer === 1 && <Icon iconName="circle" />
          }
-          {
+         {
            currentPlayer === -1 && <Icon iconName="x" />
          }
        </div>
